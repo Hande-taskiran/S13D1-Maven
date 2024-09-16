59ba2dbd4a525fe9f9e680c0fbe12c6246852b92
Hande-taskiran <handetaskiranht@gmail.com>
2024-09-16 16:54:57 +0300

    3 methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,36 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (isBarking && (clock>20 || clock<8)) {
+            return true;
+        } else if (clock<0) {
+            return false;
+        } else {
+            return false;
+        }
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if (firstAge<=19 && firstAge>=13 || secondAge<=19 && secondAge>=13 || thirdAge<=19 && thirdAge>=13) {
+           return true;
+       } else {
+            return false;
+        }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if ((isSummer && (temp<=45 && temp>=25)) || (temp<=35 && temp>=25)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static double area(double width, double height) {
-       
+
     }
 
     public static double area(double radius) {
-       
+
     }
 }
